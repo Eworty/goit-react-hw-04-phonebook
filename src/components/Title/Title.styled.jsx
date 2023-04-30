fc95481ab41b4b7a6170ae8f3af388535c6211bf
Pasha Piankovskyi <ghostangel21042017@gmail.com>
2023-04-30 06:37:02 +0300

    Add task

---
+++ src/components/Title/Title.styled.jsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const MainTitle = styled.h1`
+  text-align: center;
+  margin-bottom: 0px;
+  padding: 10px;
+  width: 400px;
+  background-color: #5f5fff;
+`;
+
+export const SecondaryTitle = styled.h2``;
